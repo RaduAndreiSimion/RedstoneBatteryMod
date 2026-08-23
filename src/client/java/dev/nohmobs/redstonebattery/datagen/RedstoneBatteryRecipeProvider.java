0aa847337553e06dbdd22875fcfc8c3d2e33579b
Radu Andrei Simion <raduandrei.simion@outlook.com>
2026-08-23 22:31:32 +0300

    Added the crafting recipe for the Redstone Battery.

---
+++ src/client/java/dev/nohmobs/redstonebattery/datagen/RedstoneBatteryRecipeProvider.java
@@ -0,0 +1,47 @@
+package dev.nohmobs.redstonebattery.datagen;
+
+import dev.nohmobs.redstonebattery.block.ModBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.concurrent.CompletableFuture;
+
+public class RedstoneBatteryRecipeProvider extends FabricRecipeProvider {
+    public RedstoneBatteryRecipeProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeProvider createRecipeProvider(HolderLookup.Provider registries, RecipeOutput output) {
+        return new RecipeProvider(registries, output) {
+            @Override
+            public void buildRecipes() {
+                HolderLookup.RegistryLookup<Item> itemLookup = registries.lookupOrThrow(Registries.ITEM);
+
+                shaped(RecipeCategory.REDSTONE, ModBlocks.REDSTONE_BATTERY)
+                        .pattern("sds")
+                        .pattern("dtd")
+                        .pattern("sds")
+                        .define('s', Blocks.STONE)
+                        .define('d', Blocks.REDSTONE_WIRE)
+                        .define('t', Items.REDSTONE_TORCH)
+                        .group("multi_bench")
+                        .unlockedBy(getHasName(Blocks.REDSTONE_WIRE), has(Blocks.REDSTONE_WIRE))
+                        .save(output);
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "RedstoneBatteryRecipeProvider";
+    }
+}
