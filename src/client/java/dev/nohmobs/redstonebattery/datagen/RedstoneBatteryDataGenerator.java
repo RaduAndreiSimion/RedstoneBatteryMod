2a78b044ecfb9ad687b2c253f4c2bb0426a3e882
Radu Andrei Simion <raduandrei.simion@outlook.com>
2026-08-23 22:02:36 +0300

    Set up Datagen

--- src/client/java/dev/nohmobs/redstonebattery/datagen/RedstoneBatteryDataGenerator.java
+++ src/client/java/dev/nohmobs/redstonebattery/datagen/RedstoneBatteryDataGenerator.java
@@ -1,4 +1,4 @@
-package dev.nohmobs.redstonebattery;
+package dev.nohmobs.redstonebattery.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
